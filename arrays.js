c73a0e261ae3af4ff13d0ec759f1f7c92ee49648
Robin Roy Julius <exo_duz@yahoo.com>
2015-04-30 14:42:09 +0800

    First commit

---
+++ arrays.js
@@ -0,0 +1,3 @@
+var pets = ['tomato sauce', 'cheese', 'pepperoni'];
+
+console.log(pets);
---
+++ if-statement.js
@@ -0,0 +1,10 @@
+var fruit = 'orange', 
+	text = '';
+
+if (fruit.length > 5) {
+	text = 'The fruit name has more than five characters.';
+} else {
+	text = 'The fruit name has five characters or less.';
+}
+
+console.log(text);
